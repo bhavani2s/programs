ab29afb83fb1f34b7afd27b92351da7eeb8929d5
bhavani2s <bhavani.202021@gmail.com>
2025-04-21 08:49:25 +0530

    Create admin_dashboard.php

---
+++ admin_dashboard.php
@@ -0,0 +1,27 @@
+<?php
+// admin_dashboard.php
+session_start();
+
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    header("Location: admin_login.php");
+    exit;
+}
+
+$admin_id = $_SESSION['admin_id'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <h2>Welcome, <?php echo $admin_id; ?>!</h2>
+    
+    <h4>Admin Dashboard</h4>
+
+    <button>
+        <a href="logout.php">Logout</a>
+    </button>
+</body>
+</html>
